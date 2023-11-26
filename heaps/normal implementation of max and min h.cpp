7a1313493d2202b6698bd3e9c14691a778fef235
priya <107272239+priya-thv@users.noreply.github.com>
2023-11-26 17:27:21 +0530

    Update normal implementation of max and min h.cpp

--- heaps/normal implementation of max and min h.cpp
+++ heaps/normal implementation of max and min h.cpp
@@ -8,8 +8,8 @@ int main() {
 	pq.push(1);
 	pq.push(2);
 	cout<<pq.top();
-	//in stl library max heap is already implemented that why we use greater<int>to reverse it into stl library basically just a method to convert min into max in stl l
-	//vector<int>is container for both min and max heap
+	//in the stl library max heap is the default creation that is why we use greater<int>to reverse it so it will create min heap stl l instead of max heap
+	//vector<int>is a container for both the min and max heap
 	priority_queue<int,vector<int>,greater<int>>pqm;
 	pqm.push(3);
 	pqm.push(1);
