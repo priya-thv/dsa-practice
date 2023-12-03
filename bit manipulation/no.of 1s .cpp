860752ddb914b0580876b3988bd2166da8b6daf1
priya <107272239+priya-thv@users.noreply.github.com>
2023-12-03 23:03:22 +0530

    Update no.of 1s .cpp

--- bit manipulation/no.of 1s .cpp
+++ bit manipulation/no.of 1s .cpp
@@ -9,3 +9,23 @@ public:
         return count;
     }
 };
+
+sol2
+#include <iostream>
+using namespace std;
+
+int numberof1(int n){
+	int count=0;
+	while(n){
+		n=n &(n-1);
+		count++;
+	}
+	return count;
+}
+
+int main() {
+	// your code goes here
+   	
+   	cout<<numberof1(7);
+	return 0;
+}
