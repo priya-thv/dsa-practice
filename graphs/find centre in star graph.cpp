2ff49b1db496a87d6e9d7ccc72dc42a818949161
priya <107272239+priya-thv@users.noreply.github.com>
2024-02-15 17:29:04 +0530

    Create find centre in star graph.cpp

---
+++ graphs/find centre in star graph.cpp
@@ -0,0 +1,19 @@
+
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        map<int,int>mp;
+        for(int i=0;i<edges.size();i++){
+            mp[edges[i][0]]++;
+            mp[edges[i][1]]++;
+        }
+        
+        int t;
+        for(auto x:mp){
+            if(x.second==edges.size()){
+                t=x.first;  
+            }
+        }
+        return t;
+    }
+};
