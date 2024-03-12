7377d4e8f351cab1cc98be97f8f1ba34688df447
priya <107272239+priya-thv@users.noreply.github.com>
2024-03-12 13:51:26 +0530

    Create remove zero sum sublists.cpp

---
+++ daily question of leetcode/remove zero sum sublists.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeZeroSumSublists(ListNode* head) {
+        ListNode*h=new ListNode(0);
+        h->next=head;
+        int pre=0;
+        map<int,ListNode*>seen;
+        for(auto i=h;i;i=i->next){
+            seen[pre+=i->val]=i;
+        }
+        pre=0;
+        for(auto i=h;i;i=i->next){
+            i->next=seen[pre+=i->val]->next;
+        }
+        return h->next;
+
+    }
+};
+
