403722d5619dada73ed49f6b05c945c8ecc2d032
priya <107272239+priya-thv@users.noreply.github.com>
2024-03-28 16:29:56 +0530

    Update nth fibonaci number.cpp

--- Dynamic Programming/nth fibonaci number.cpp
+++ Dynamic Programming/nth fibonaci number.cpp
@@ -41,3 +41,25 @@ int main(){
         }
         cout<<dp[n]<<endl;
 }
+
+
+// space  optimization
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+        int n;
+        cin>>n;
+        // vector<int>dp(n+1);
+        int preve=0;
+       int p=1;
+       int t=-1;
+        for(int i=2;i<=n;i++){
+                t=preve+p  ;
+                preve=p;
+                p=t;
+        }
+        cout<<p<<endl;
+}
