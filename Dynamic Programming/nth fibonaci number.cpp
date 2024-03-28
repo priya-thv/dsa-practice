695b388e9fe96f850c7879fb5d437746bea78962
priya <107272239+priya-thv@users.noreply.github.com>
2024-03-28 16:13:30 +0530

    Create nth fibonaci number.cpp

---
+++ Dynamic Programming/nth fibonaci number.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int fib(int n, vector<int>& dp) {
+    if(n == 0)
+        return 0;
+    if(n == 1)
+        return 1;
+    if(dp[n] != -1)
+        return dp[n];
+    
+    dp[n] = fib(n-1, dp) + fib(n-2, dp);
+    return dp[n];
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> dp(n + 1, -1);
+    cout << fib(n, dp) << endl;
+    return 0;
+}
