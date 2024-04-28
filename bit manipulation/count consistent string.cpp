935445263d5a2acf08c1159e86506b9ca1000bb3
priya <107272239+priya-thv@users.noreply.github.com>
2024-04-28 17:01:08 +0530

    Update count consistent string.cpp

--- bit manipulation/count consistent string.cpp
+++ bit manipulation/count consistent string.cpp
@@ -20,3 +20,36 @@ public:
         return res;
     }
 };
+
+
+// bit manipulation
+
+class Solution {
+public:
+    int countConsistentStrings(string allowed, vector<string>& words) {
+        int res=0;
+        int flag=0;
+        for(int i=0;i<allowed.size();i++){
+            int val=allowed[i]-'a';
+            flag=flag | (1<<val);
+        }
+        for(int i=0;i<words.size();i++){
+            bool t=true;
+            for(int j=0;j<words[i].size();j++){
+                int val=words[i][j]-'a';
+                if((flag & (1<<val))){
+                    cout<<(flag & (1<<val))<<endl;
+                    continue;
+                }else{
+                    t=false;
+                    break;
+                }
+            }
+            if(t==true){
+                res++;
+            }
+
+        }
+        return res;
+    }
+};
