aed50d7fe754563ce490141158eef1bc97c625aa
priya <107272239+priya-thv@users.noreply.github.com>
2024-05-01 12:35:31 +0530

    Create Minimum one bit operations.cpp

---
+++ bit manipulation/Minimum one bit operations.cpp
@@ -0,0 +1,25 @@
+#define ll long long int
+class Solution {
+public:
+    int minimumOneBitOperations(int n) {
+        vector<ll>f(31,0);
+        f[0]=1;
+
+        for(int i=1;i<=30;i++){
+            f[i]=2*f[i-1]+1;
+        }
+
+        int ans=0,count=0;
+        for(int i=30;i>=0;i--){
+            if((1 << i) & n){
+                count++;
+                if(count%2){
+                    ans+=f[i];
+                }else{
+                    ans-=f[i];
+                }
+            }
+        }
+        return ans;
+    }
+};
