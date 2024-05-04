3b453a8be6d17c24f756c385c2bcee771a53e068
priya <107272239+priya-thv@users.noreply.github.com>
2024-05-04 11:25:50 +0530

    Create sort integer by the number of 1 bits.cpp

---
+++ bit manipulation/sort integer by the number of 1 bits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> sortByBits(vector<int>& arr) {
+        map<int,list<int>>mp;
+        for(auto t:arr){
+            int v=__builtin_popcount(t);
+            mp[v].push_back(t);
+        }
+        vector<int>v;
+       
+        for(auto t:mp){
+            vector<int> temp(t.second.begin(), t.second.end());
+            sort(temp.begin(), temp.end());
+            for (auto j : temp) {
+                v.push_back(j);
+            }
+        }
+        return v;
+    }
+};
